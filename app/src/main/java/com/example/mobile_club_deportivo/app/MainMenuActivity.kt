5fdcebfd5b69b85e6282be4e4523805a6ac34be4
BrianSabio <Brian232245@gmail.com>
2026-05-09 06:51:34 -0400

    feat: Implementar layout de menu principal

---
+++ app/src/main/java/com/example/mobile_club_deportivo/app/MainMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mobile_club_deportivo.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
