2db1e0a561ac0181f75d54860aa78845c9d14f83
BrianSabio <Brian232245@gmail.com>
2026-05-09 11:10:37 -0400

    feat: Actualizar layout de menu principal y login

--- app/src/main/java/com/example/mobile_club_deportivo/app/MainMenuActivity.kt
+++ app/src/main/java/com/example/mobile_club_deportivo/app/MainMenuActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        setContentView(R.layout.activity_main_menu)
     }
 }
